3224171d82ae2f31ca1d7d2b8d74877ad1fb33c8
Francesca Dreith <francesca.dreith@gmail.com>
2019-11-09 18:31:19 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,8 +31,8 @@ def array_literal_matrix
   # alphabetically by the first character.
   
   array_literal_matrix = [
-    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
-    ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
+    ["Bruce Green", "Don Gately", "Joelle van Dyne", "Kate Gompert", "Pat M."],
+    ["Gerhard Schtitt", "Hal Incandenza", "Lyle",  "Mario Incandenza", "Michael Pemulis"]
     ]
 end
 
